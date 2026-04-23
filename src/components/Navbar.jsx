e20b8180acd8e55b38d4abe3ba2a4efdb9b52ffb
AxelXrest <axelxrest@gmail.com>
2026-04-23 11:51:18 +0545

    rollback the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { styles } from "../styles";
 import { Link } from 'react-router-dom';
 
@@ -15,7 +15,7 @@ const Navbar = () => {
 
     return (
         <nav className={`
-        ${styles.paddingX} w-full flex items-center py-4 fixed top-0 z-20 backdrop-blur-lg bg-[#07070799]`}>
+        ${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 backdrop-blur-lg bg-[#07070799]`}>
             <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
                 <Link
                     to="/"
@@ -25,9 +25,9 @@ const Navbar = () => {
                         window.scrollTo(0, 0);
                     }}>
                     <img src={logo} alt="logo" className='w-10 h-10 object-contain' />
-                    <p className='text-white text-[16px] sm:text-[18px] font-semibold hidden xs:flex'>Ajaya Nemkul Shrestha</p>
+                    <p className='text-white text-[18px] font-semibold flex cursor-pointer]'>Ajaya Nemkul Shrestha</p>
                 </Link>
-                <ul className='list-none hidden sm:flex flex-row gap-6 lg:gap-10'>
+                <ul className='list-none hidden sm:flex flex-row gap-10'>
                     {navLinks.map((Link) => (
                         <li
                             key={Link.id}
@@ -55,7 +55,7 @@ const Navbar = () => {
                          className='w-[28px] h-[28px] object-contain cursor-pointer'
                          onClick={() => setToggle(!toggle)}
                     />
-                    <div className={`${!toggle ? 'hidden' : 'flex'} backdrop-blur-[12px] p-6 bg-gradient-to-b from-[#000000d0] shadow-card to-[#00000051] border-[1px] border-gray-500 absolute top-20 left-2 right-2 my-2 z-10 rounded-xl`}>
+                    <div className={`${!toggle ? 'hidden' : 'flex'} backdrop-blur-[12px] p-6 bg-gradient-to-b from-[#000000d0] shadow-card to-[#00000051] border-[1px] border-gray-500 absolute top-20 right-0 mx-4 my-2 min-w[140px] z-10 rounded-xl`}>
                         <ul className='list-none flex justify-end items-start flex-col gap-4'>
                             {navLinks.map((Link) => (
                                 <li
